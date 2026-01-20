54b1fa40b1795edfea9d413d1b4c99eb25d49e87
Shishir Goel <shishir.ace@gmail.com>
2026-01-20 09:13:01 +0100

    Fixed missing positive number

--- arrays/src/main/java/missing/minimum/positive/number/MissingNumber.java
+++ arrays/src/main/java/missing/minimum/positive/number/MissingNumber.java
@@ -1,27 +1,35 @@
 package missing.minimum.positive.number;
 
+import java.util.Arrays;
+
 /**
  * Given an array with positive, negative and duplicate numbers
  * find the missing minimum positive number in linear time and constant space (O(n) & space to store 1 number)
  */
 public class MissingNumber {
     public static void main(String[] args) {
-//        int[] inputArray = {3,4,-1,1};
+        int[] inputArray = {3,2,6,4,1,0};
+//        int[] inputArray = {3,2,-1,1};
 //        int[] inputArray = {1,2,0};
-        int[] inputArray = {1,2,3};
+//        int[] inputArray = {1,2,3};
         System.out.println(findMissingMinimumPositiveNumber(inputArray));
     }
 
     private static int findMissingMinimumPositiveNumber(int[] inputArray) {
-        int missingNumber = 1; // 1 is the minimum positive number
-        for(int i : inputArray) {
-            if(i == missingNumber) {
-                missingNumber++;
-            }
-        }
-        if(missingNumber > inputArray.length) {
-            return -1;
-        }
-        return missingNumber;
+       boolean[] available = new boolean[inputArray.length];
+       Arrays.fill(available, false);
+       for(int i:inputArray) {
+           if( i > 0 && i <= inputArray.length) {
+               available[i-1] = true;
+           }
+       }
+       int j = 0;
+       for(boolean b:available) {
+           if(!b) {
+               return j+1;
+           }
+           j++;
+       }
+       return j+1;
     }
 }
